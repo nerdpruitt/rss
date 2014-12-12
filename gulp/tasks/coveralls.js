ea86bf952b32523153e2af245d17d9ba3ef0f933
Nick Pruitt <pruitt.nick@gmail.com>
2014-12-12 10:15:28 -0600

    Revert "Gulp coveralls task"
    This reverts commit 8ed8aa6742b87bbfa198087448ff7b5bc778b7bf.

--- gulp/tasks/coveralls.js
+++ gulp/tasks/coveralls.js
@@ -2,6 +2,6 @@ var gulp = require('gulp'),
     coveralls = require('gulp-coveralls');
 
 gulp.task('coveralls', function() {
-    return gulp.src('./coverage/lcov.info')
+    return gulp.src('./coverage/**/lcov.info')
             .pipe(coveralls());
 });
